5fd3d3b5f2af275c8746ba64681e71825e04cfb7
BJ <ewozniakblazej@gmail.com>
2022-06-06 22:52:47 +0200

    http/ Async / Await & FutureBuilder

--- lib/Message.dart
+++ lib/Message.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class Message {
   String subject;
@@ -9,6 +13,19 @@ class Message {
       : subject = json ['subject'],
         body = json['body'];
 
+  static Future<List<Message>> browse()  async{
+    // String content = await rootBundle.loadString('data/message.json');
+    http.Response response =
+    await http.get(Uri.parse(Uri.encodeFull('https://run.mocky.io/v3/d20a2a7b-99a8-4f70-a32d-b9c58e29cfa1')));
 
+    await Future.delayed(Duration(seconds:2));
+
+    String content = response.body;
+    List collection = json.decode(content);
+    List<Message> _messages = collection.map((json) => Message.fromJson(json)).toList();
+
+    // print(content);
+    return _messages;
+  }
 }
 
--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -1,7 +1,9 @@
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:core';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'Message.dart';
 
@@ -14,46 +16,81 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
-  List<Message> messages = [];
+   late Future<List<Message>> messages;
+                                                                                   // bool isLoading = true;
 
-  Future loadMessageList() async{
-    // String content = await rootBundle.loadString('data/message.json');
-    http.Response response =
-      await http.get(Uri.parse(Uri.encodeFull('https://run.mocky.io/v3/d20a2a7b-99a8-4f70-a32d-b9c58e29cfa1')));
-    String content = response.body;
-    List collection = json.decode(content);
-    List<Message> _messages = collection.map((json) => Message.fromJson(json)).toList();
-
-    // print(content);
-    setState(() {
-      messages = _messages;
-    });
-  }
+                                                                                  //  Future loadMessageList() async{
+                                                                                  // String content = await rootBundle.loadString('data/message.json');
+                                                                                  // List<Message> _messages = await Message.browse();
+   // setState(() {
+    //  messages = _messages;
+                                                                                  // isLoading = false;
+  //  });
+ // }
 
   void initState() {
-    loadMessageList();
+                                                                                  // loadMessageList();
     super.initState();
+    messages = Message.browse();
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        separatorBuilder: (context, index) => Divider(),
-        itemBuilder: (BuildContext context, int index ) {
-          Message message = messages[index];
-          return ListTile(
-            title: Text(message.subject),
-            subtitle: Text(message.body),
-
-            leading: CircleAvatar(backgroundColor: Colors.cyan,
-                child: Text('$index')),
-          );
+        actions: <Widget>[IconButton(onPressed: (){
+          var _messages = Message.browse();
+          setState(() {
+            messages = _messages;
+          });
         },
+            icon: Icon(Icons.refresh))],
       ),
+      body: FutureBuilder (
+        future: messages,
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          switch (snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              var messages = snapshot.data;
+              return ListView.separated(
+                    itemCount: messages.length,
+                    separatorBuilder: (context, index) => Divider(),
+                    itemBuilder: (BuildContext context, int index ) {
+                      Message message = messages[index];
+                      return ListTile(
+                        title: Text(message.subject),
+                        subtitle: Text(message.body),
+
+                        leading: CircleAvatar(backgroundColor: Colors.cyan,
+                            child: Text('$index')),
+                      );
+                    },
+                  );
+
+          }
+
+        },
+      )
+
+      //isLoading?Center(child: RefreshProgressIndicator()):
+      // ListView.separated(
+      //   itemCount: messages.length,
+      //   separatorBuilder: (context, index) => Divider(),
+      //   itemBuilder: (BuildContext context, int index ) {
+      //     Message message = messages[index];
+      //     return ListTile(
+      //       title: Text(message.subject),
+      //       subtitle: Text(message.body),
+      //
+      //       leading: CircleAvatar(backgroundColor: Colors.cyan,
+      //           child: Text('$index')),
+      //     );
+      //   },
+      // ),
     );
   }
 }
